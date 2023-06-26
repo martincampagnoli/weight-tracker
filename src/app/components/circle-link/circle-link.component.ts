7be559f9983639dfbe673daeadd5dad3acb2c617
Martin Campagnoli <martin.campagnoli@incentro.com>
2023-06-26 16:58:47 +0200

    Remove unused oninit

--- src/app/components/circle-link/circle-link.component.ts
+++ src/app/components/circle-link/circle-link.component.ts
@@ -1,16 +1,14 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-circle-link',
   templateUrl: './circle-link.component.html',
   styleUrls: ['./circle-link.component.scss'],
 })
-export class CircleLinkComponent implements OnInit {
+export class CircleLinkComponent {
   @Input() title: string | undefined;
   @Input() link: string | undefined;
   @Input() img: string | undefined;
 
   constructor() {}
-
-  ngOnInit() {}
 }
